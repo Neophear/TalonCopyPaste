bf0fb4c24ddc0f4a42bb1a8ca0522a0235f19d4a
Neophear <stiiggade@gmail.com>
2019-10-11 11:03:02 +0200

    Initial commit

---
+++ TalonCopyPaste/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TalonCopyPaste
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AllianceNoWarmode(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText("Atalonfarmer-Shadowsong");
+        }
+
+        private void AllianceWarmode(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText("Aptalonfarm-Shadowsong");
+        }
+
+        private void HordeNoWarmode(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText("Htalonfarmer-Shadowsong");
+        }
+
+        private void HordeWarmode(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText("Hptalonfarm-Shadowsong");
+        }
+    }
+}
